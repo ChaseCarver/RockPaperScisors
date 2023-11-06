8f2097d3ee67d33084632520bf15076d02a2c23e
ChaseCarver <143764924+ChaseCarver@users.noreply.github.com>
2023-11-06 09:51:09 -0600

    s js

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -4,28 +4,28 @@ var outputOutcome = document.getElementById("choiceOutcome")
 var outputComputer = document.getElementById("computer");
 var outputPlayer = document.getElementById("player");
 
-function rockpaperscisors(rps){
+function rockpaperscissors(rps){
 //produce a random number 0, 1 or 2
   var rpsRandom = (Math.floor(Math.random(2) * 3))
-//switch into plaintext 0= rock 1= paper 2=scisors
+//switch into plaintext 0= rock 1= paper 2=scissors
 switch (rpsRandom) {
   case 0: rpsRandom = "rock";
    break;
   case 1: rpsRandom = "paper";
    break;
-  case 2: rpsRandom = "scisors";
+  case 2: rpsRandom = "scissors";
    break;
 }
 //compare inputs, add winner to scoreboard, update outcome text
 if(rps == rpsRandom){
  outputOutcome.textContent = "I also chose " + rpsRandom + ", Tie! Try again!";
 }
-else if (rpsRandom == "paper" && rps == "rock" || rpsRandom == "rock" && rps == "scisors" || rpsRandom == "scisors" && rps == "paper"){
+else if (rpsRandom == "paper" && rps == "rock" || rpsRandom == "rock" && rps == "scissors" || rpsRandom == "scissors" && rps == "paper"){
  computer++
  outputOutcome.textContent = "I choose " + rpsRandom + ", I win!";
  outputComputer.textContent = computer;
 }
-else if (rpsRandom == "scisors" && rps == "rock" || rpsRandom == "paper" && rps == "scisors" || rpsRandom == "rock" && rps == "paper"){
+else if (rpsRandom == "scissors" && rps == "rock" || rpsRandom == "paper" && rps == "scissors" || rpsRandom == "rock" && rps == "paper"){
  console.log("I choose " + rpsRandom + "!")
  console.log("You win!")
  player++
@@ -36,10 +36,10 @@ else if (rpsRandom == "scisors" && rps == "rock" || rpsRandom == "paper" && rps 
 //link button to js
 //on button click, run corresponding input through the function
 var rockClick = document.getElementById("rockButton")
-rockButton.addEventListener("click", function(a){rockpaperscisors("rock")})
+rockButton.addEventListener("click", function(a){rockpaperscissors("rock")})
 
 var paperClick = document.getElementById("paperButton")
-paperButton.addEventListener("click", function(a){rockpaperscisors("paper")})
+paperButton.addEventListener("click", function(a){rockpaperscissors("paper")})
 
-var scisorsClick = document.getElementById("scisorsButton")
-scisorsButton.addEventListener("click", function(a){rockpaperscisors("scisors")})
+var scissorsClick = document.getElementById("scissorsButton")
+scissorsButton.addEventListener("click", function(a){rockpaperscissors("scissors")})
